a8ecdfc6869ddd8780c6e00297f3322034b400cb
unknown <no.trbl.2.u@gmail.com>
2019-01-16 17:06:56 -0500

    Finishe Hello Word & Leap

---
+++ haskell/leap/src/LeapYear.hs
@@ -0,0 +1,17 @@
+-- module LeapYear (isLeapYear, isDivBy, isNotDivBy) where
+module LeapYear where
+
+isDivBy :: Int -> (Int -> Bool)
+isDivBy n = \m -> (mod) m n == 0
+
+isNotDivBy :: Int -> (Int -> Bool)
+isNotDivBy n = not . isDivBy n
+
+isLeapYear :: Int -> Bool
+isLeapYear year
+  | (isNotDivBy 4) year                         = False
+  | (isDivBy 4) year && (isNotDivBy 100) year   = True
+  | (isDivBy 100) year && (isNotDivBy 400) year = False
+  | (isDivBy 400) year                          = True
+  | (isDivBy 200) year && (isNotDivBy 400) year = False
+  | otherwise                                   = False
